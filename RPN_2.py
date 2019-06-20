3d9a0d03a02c35eb686389a44b59811786b44382
idnb <elhanan18@gmail.com>
2019-06-20 20:02:40 +0300

    rpn

--- RPN_2.py
+++ RPN_2.py
@@ -5,13 +5,19 @@ class Calculator:
     def __init__(self):
         self._expression = []
         self._variables = {}
+        self._op_dict = {
+            '+': lambda a, b: a + b,
+            '-': lambda a, b: a - b,
+            '*': lambda a, b: a * b,
+            '/': lambda a, b: a / b,
+        }
 
     def run(self):
-        self._handle_expression()
+        self._expression = self._handle_expression()
         stop = False
         while not stop:
             try:
-                print("\nExpression: {}\nInsert variables:".format(self._expression))
+                print("Expression: {}\nInsert variables:".format(self._expression))
                 variables_str = input().strip()
                 self._parse_variables_input(variables_str)
                 res = self._calc_result()
@@ -21,21 +27,28 @@ class Calculator:
         print("Good Bye")
 
     def _handle_expression(self):
-        for item in sys.argv[1].strip().split():
+        assert (len(sys.argv) == 2)
+        expression_str = sys.argv[1].strip().split()
+        expression = []
+        # simplify expression
+        print("\nExpression: {}".format(expression_str))
+        for item in expression_str:
             if self._is_operand(item):
                 if self._is_digit(item):
                     item = int(item)
-                self._expression.append(item)
+                expression.append(item)
             else:  # operator
-                assert (len(self._expression) >= 2)
-                operand_2, operand_1 = self._expression.pop(), self._expression.pop()
-                if self._is_digit(operand_1) and self._is_digit(operand_2):
-                    tmp_res = self._calc_tmp_exp(operand_1, operand_2, item)
-                    self._expression.append(tmp_res)
+                assert (len(expression) >= 2)
+                operator = item
+                operand_2, operand_1 = expression.pop(), expression.pop()
+                if type(operand_1) == int and type(operand_2) == int:
+                    tmp_res = self._op_dict[operator](operand_1, operand_2)
+                    expression.append(tmp_res)
                 else:
-                    self._expression.append(operand_1)
-                    self._expression.append(operand_2)
-                    self._expression.append(item)
+                    expression.append(operand_1)
+                    expression.append(operand_2)
+                    expression.append(operator)
+        return expression
 
     def _parse_variables_input(self, variables_str):
         if not variables_str:
@@ -47,10 +60,8 @@ class Calculator:
 
     def _calc_result(self):
         stack = []
-        for item in self._expression.split():
+        for item in self._expression:
             if self._is_operand(item):
-                if self._is_digit(item):
-                    item = int(item)
                 stack.append(item)
             else:  # operator
                 assert (len(stack) >= 2)
@@ -61,17 +72,12 @@ class Calculator:
         return stack.pop()
 
     def _calc_tmp_exp(self, operand_1, operand_2, operator):
-        if type(operand_1) is str:
+        if operand_1 in self._variables:
             operand_1 = self._variables[operand_1]
-        if type(operand_2) is str:
+        if operand_2 in self._variables:
             operand_2 = self._variables[operand_2]
 
-        return {
-            '+': operand_1 + operand_2,
-            '-': operand_1 - operand_2,
-            '*': operand_1 * operand_2,
-            '/': operand_1 / operand_2,
-        }[operator]
+        return self._op_dict[operator](operand_1, operand_2)
 
     @staticmethod
     def _is_digit(item):
